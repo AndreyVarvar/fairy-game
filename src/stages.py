552196de18236f6efe7de6b56d9556d884aa002f
Yeats Aiden <ayeats097@gmail.com>
2026-04-14 18:00:10 +0200

    you have a cursed library, but now it is fixed - courtesy of Aiden the great

--- src/stages.py
+++ src/stages.py
@@ -1,5 +1,5 @@
 from prismane import Stage
-from .ui import StartButton
+from prismane.ui import Button
 
 import pygame as pg
 
@@ -7,7 +7,10 @@ import pygame as pg
 class MainMenuStage(Stage):
     def __init__(self):
         super().__init__()
-        self.populate_group("ui", StartButton(pg.Rect(100, 100, 100, 100), 1))
+        self.populate_group("ui", Button(pg.Rect(100, 100, 100, 100), 1, lambda: self.queue_next_scene("game")))
 
 
-
+class GameStage(Stage):
+    def __init__(self):
+        super().__init__()
+        pass
--- src/ui.py
+++
@@ -1,9 +0,0 @@
-from prismane.ui import Button
-
-import pygame as pg
-
-
-class StartButton(Button):
-    def __init__(self, rect: pg.Rect, z=1):
-        super().__init__(rect, z)
-
