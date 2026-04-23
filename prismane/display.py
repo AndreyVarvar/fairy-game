0806685bda5f7f0f89fbad0f4e4eace1950eb077
Yeats Aiden <ayeats097@gmail.com>
2026-04-23 19:41:03 +0300

    lame save

--- prismane/display.py
+++ prismane/display.py
@@ -1,28 +1,16 @@
 import pygame as pg
 
-from .entity import Entity
+from .element import Element
 from .panels import InputControlPanel, TimeControlPanel
 
-class Mover(Entity):
-    def __init__(self, x: int, y: int) -> None:
-        super().__init__()
-        self.pos.update(x, y)
 
-    def move(self) -> None:
-        input_panel: InputControlPanel = self.element_tree["InputControlPanel"]
-        game_panel: TimeControlPanel = self.element_tree["TimeControlPanel"]
-        dt = game_panel.dt
-        self.pos.y -= input_panel.keys_pressed[pg.K_UP] * 500 * dt
-        self.pos.y += input_panel.keys_pressed[pg.K_DOWN] * 500 * dt
-        self.pos.x += input_panel.keys_pressed[pg.K_RIGHT] * 500 * dt
-        self.pos.x -= input_panel.keys_pressed[pg.K_LEFT] * 500 * dt
-
-
-class Display(Entity):
+class Display(Element):
     def __init__(self, window: pg.Surface, width: int, height: int, **kwargs) -> None:
-        super().__init__(True)
+        super().__init__(singleton=True)
         self.window = window
         self.image = pg.Surface((width, height), kwargs["flags"] if "flags" in kwargs else 0)
+        self.size = pg.Vector2()
+        self.pos = pg.Vector2()
         self.scale: float = min(self.window.width/self.image.width, self.window.height/self.image.height)
         self.frame: pg.Surface
         self.z = -1
@@ -31,9 +19,9 @@ class Display(Entity):
         # scales up the display up to the window size.
         self.scale = min(self.window.get_width() / self.image.get_width(), self.window.get_height() / self.image.get_height())
         self.frame = pg.transform.scale_by(self.image, self.scale)
-        self.size = self.frame.size
         self.pos.x, self.pos.y = (self.window.get_width() - self.frame.get_width()) // 2, (self.window.get_height() - self.frame.get_height()) // 2
+        self.size.update(*self.frame.size)
 
     def queue_draw(self) -> None:
-        self.element_tree["Renderer"].queue_draw(self.frame, self.z, self.target, self.pos)
+        self.element_tree["Renderer"].queue_draw(self.frame, self.z, "window", self.pos)
 
